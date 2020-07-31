1b58c6b9a043e4e8e95774f1fc82f9a7004c9a0b
joejons55 <vladomel55@gmail.com>
2020-07-31 16:34:33 -0700

    adds routs.js

---
+++ routs/routs.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const itemsController = require('./controllers/items');
+const listsController = require('./controllers/lists');
+// const authController = require('./controllers/authController');
+
+// Lists Routes
+app.use('/lists', listsController);
+
+// Items Routes
+app.use('/items', itemsController);
+
+// Auth Routes
+app.use('/auth', authController);
+
+
+// ------------------------ API ROUTES
+
+
+// Home Page Route
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+module.exports = router
