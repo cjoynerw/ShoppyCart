8427fe31f09f6dd031ee8da5ea5d4fdfc5cfffb1
Matthew Neal <mneal.jw@gmail.com>
2020-07-31 17:59:51 -0400

    I added the vews folders for the items and the lists

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 require('./db/db')
 
 
+app.use(methodOverride('_method'))
+
+app.use(express.urlencoded({ extended: false })))
+
+//ROUTES
 
 app.listen(3000, () => {
     console.log("I'm listening")
