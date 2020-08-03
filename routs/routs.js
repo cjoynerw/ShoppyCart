6421b0c33b23c3e4cba1292e79e343e8a8d8f5eb
joejons55 <vladomel55@gmail.com>
2020-08-03 11:39:41 -0700

    update

--- routs/routs.js
+++ routs/routs.js
@@ -1,30 +1,30 @@
 const express = require('express')
 const router = express.Router()
-const itemsController = require('./controllers/items');
-const listsController = require('./controllers/lists');
+// const itemsController = require('./controllers/items');
+// const listsController = require('./controllers/lists');
 // const authController = require('./controllers/authController');
 
 // Lists Routes
-app.use('/lists', listsController);
+// app.use('/lists', listsController);
 
-// Items Routes
-app.use('/items', itemsController);
+// // Items Routes
+// app.use('/items', itemsController);
 
-// Auth Routes
+// // Auth Routes
 
-app.use('/auth', authController);
+// app.use('/auth', authController);
 
 // ------------------------ API ROUTES
 
 
 // Home Page Route
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
 // Grocery Stores Route
-app.get('/', (req, res) => {
-    res.render(stores)
+router.get('/', (req, res) => {
+    res.render('stores')
 })
 
 module.exports = router
