4b27d40ecd0d37f7ee44e126de94d55b6d92a438
cjoynerw <cjoynerw@gmail.com>
2020-08-03 15:45:53 -0500

    Added Models

---
+++ models/items.js
@@ -0,0 +1,17 @@
+// REQUIRES
+const mongoose = require('mongoose')
+
+// SCHEMA
+const itemSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    unit: String,
+    notes: String,
+    date: String,
+})
+
+// CREATE MODEL
+const Item = mongoose.model("Item", itemSchema)
+
+// EXPORT
+module.exports = Item
